db95828d74d7589dedb1d0653907fc49ada4ea99
Vishesh <visheshsingh16@gmail.com>
2017-12-22 00:29:44 -0500

    Find digit position in a number

---
+++ DigitPosition.java
@@ -0,0 +1,25 @@
+
+public class DigitPosition {
+
+	public static void main(String[] args) {
+		System.out.println(getPosition(2017,7));
+	}
+	
+	public static int getPosition(int num, int n) {
+		if(n<0) {
+			return -1;
+		} 
+		if(n>9) {
+			return -2;
+		} 
+		int position = 1;
+		while(num != 0) {
+			int rem = num % 10;
+			if(rem == n) break;
+			num = num / 10;
+			position = position * 10;
+		}
+		return position;
+	}
+
+}
